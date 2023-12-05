9d2904d8de0097f0e023058a3b7fbb516f37d26e
Kimuzeunyon <60294084+XgitalBounce@users.noreply.github.com>
2023-12-06 08:18:30 +0900

    계정 도메인 추가 작업
    인증 기능 구현을 위해 계정 도메인을 추가한 작업

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -16,7 +16,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
@@ -25,52 +25,70 @@ import java.util.Set;
 })
 @EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article extends AuditingFields{
+public class Article extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter @Column(nullable = false) private String title; //제목
-    @Setter @Column(nullable = false, length = 10000) private String content; // 본문
+    @Setter
+    @ManyToOne(optional = false)
+    private UserAccount userAccount; // 유저 정보 (ID)
 
-    @Setter private String hashtag; // 해시태그
+    @Setter
+    @Column(nullable = false)
+    private String title; //제목
+    @Setter
+    @Column(nullable = false, length = 10000)
+    private String content; // 본문
+
+    @Setter
+    private String hashtag; // 해시태그
 
     @ToString.Exclude
-    @OrderBy("id")
+    @OrderBy("createdAt DESC")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>(); // One to Many 관계 설정
 
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy @Column(nullable = false, length=100) private String createdBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy @Column(nullable = false,length=100) private String modifiedBy; // 수정자
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt; // 생성일시
+    @CreatedBy
+    @Column(nullable = false, length = 100)
+    private String createdBy; // 생성자
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
+    private String modifiedBy; // 수정자
 
 
     protected Article() { //기본 생성자 (평소에 사용 X)
 
     }
 
-    private Article(String title, String content, String hashtag) {
+    private Article(UserAccount userAccount, String title, String content, String hashtag) {
+        this.userAccount = userAccount;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
 
-    public static Article of(String title, String content, String hashtag) {
-        return new Article(title, content, hashtag);
-    }
+    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
+        return new Article(userAccount, title, content, hashtag);}
 
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals (Object o){
         if (this == o) return true;
         if (!(o instanceof Article article)) return false;
         return id != null && id.equals(article.id);
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode () {
         return Objects.hash(id);
     }
 }
+
