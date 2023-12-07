982183809e4b8321f7a471b741c48468f6d04db9
Kimuzeunyon <60294084+XgitalBounce@users.noreply.github.com>
2023-12-07 15:46:51 +0900

    #18- 게시판 컨트롤러 버그 수정
    articleService.searchArticles가 null로 삽입되는 부분 수정

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -5,7 +5,9 @@ import com.fastcampus.projectboard.domain.type.SearchType;
 import com.fastcampus.projectboard.dto.response.ArticleResponse;
 import com.fastcampus.projectboard.dto.response.ArticleWithCommentsResponse;
 import com.fastcampus.projectboard.service.ArticleService;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -16,21 +18,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
-    @MockBean
-    private ArticleService articleService;
+    private final ArticleService articleService;
 
     @GetMapping
-    public String articles(@RequestParam(required = false)SearchType searchType,
-                           @RequestParam(required = false)String searchValue,
+    public String articles(@RequestParam(required = false) SearchType searchType,
+                           @RequestParam(required = false) String searchValue,
                            @PageableDefault(size=10, sort="createdAt", direction = Sort.Direction.DESC) Pageable pageable,
                            ModelMap map)
     {
-
-        map.addAttribute("articles", articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from));
+         map.addAttribute("articles", articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from));
+//        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
         return "articles/index";
     }
     @GetMapping("/{articleId}")
